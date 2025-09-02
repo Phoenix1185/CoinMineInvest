ffd0485c9d74871d2a775d2a7b1b5d3a0b82215c
Phoenix1185 <fredokcee1@gmail.com>
2025-09-03 00:28:58 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,30 +6,29 @@ import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 export default defineConfig({
   plugins: [
     react(),
-    ...(process.env.NODE_ENV !== "production" ? [runtimeErrorOverlay()] : []),
-    ...(process.env.NODE_ENV !== "production" && process.env.REPL_ID
+    runtimeErrorOverlay(),
+    ...(process.env.NODE_ENV !== "production" &&
+    process.env.REPL_ID !== undefined
       ? [
-          import("@replit/vite-plugin-cartographer").then((m) =>
-            m.cartographer()
+          await import("@replit/vite-plugin-cartographer").then((m) =>
+            m.cartographer(),
           ),
         ]
       : []),
   ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./client/src"),
-      "@shared": path.resolve(__dirname, "./shared"),
-      "@assets": path.resolve(__dirname, "./attached_assets"),
+      "@": path.resolve(import.meta.dirname, "client", "src"),
+      "@shared": path.resolve(import.meta.dirname, "shared"),
+      "@assets": path.resolve(import.meta.dirname, "attached_assets"),
     },
   },
-  root: path.resolve(__dirname, "./client"),
+  root: path.resolve(import.meta.dirname, "client"),
   build: {
-    outDir: path.resolve(__dirname, "./dist"),
+    outDir: path.resolve(import.meta.dirname, "dist/public"),
     emptyOutDir: true,
-    sourcemap: false,
   },
   server: {
-    port: 5000,
     fs: {
       strict: true,
       deny: ["**/.*"],
